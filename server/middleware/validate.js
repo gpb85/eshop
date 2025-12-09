e61fb13a4e6a45eff21f95f160f7775f2ad2b84b
GpB <giorgos_bithas@hotmail.com>
2025-12-10 00:00:41 +0200

    again

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -1,6 +1,7 @@
 export const validate = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body, { abortEarly: false });
+
     if (error) {
       const messages = error.details.map((detail) => detail.message);
       return res.status(400).json({ success: false, errors: messages });
